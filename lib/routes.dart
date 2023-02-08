6d83ef6f42569d85b2a4023ee803578d6a8e645b
siddhanthole74 <siddhanthole74@gmail.com>
2023-02-08 09:14:35 +0530

    First Commit  Design Part

---
+++ lib/routes.dart
@@ -0,0 +1,16 @@
+
+
+
+
+
+//import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  //Splash.routeName:(context)=>Splash(),
+  //SignInScreen.routeName:(context)=>SignInScreen(),
+
+
+
+};
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,10 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  flutter_secure_storage_windows
+  geolocator_windows
+  smart_auth
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
