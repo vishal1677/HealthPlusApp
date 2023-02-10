12c4ad653961a095e677c1bb8e6bab58b7be6427
Manish <manishgodbole022@gmail.com>
2023-02-10 14:48:48 +0530

    Otp state change

---
+++ lib/ReportDisplay/UserReport.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/cupertino.dart';
+//
+// class UserReport extends StatefulWidget{
+//   @override
+//   State<UserReport> createState() {
+//     return UserReportState();
+//   }
+// }
+// class OtpState extends State<OtpScreen>{
+//   String otpCode="";
+//   String Checkotp="";
+//   final FirebaseAuth auth=FirebaseAuth.instance;
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return Scaffold(
