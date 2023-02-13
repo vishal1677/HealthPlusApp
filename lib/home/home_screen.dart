da7243efb813da25b6a3a33f83d713f7b3a67f40
siddhanthole74 <siddhanthole74@gmail.com>
2023-02-13 16:20:45 +0530

    final banner

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:healthplus/home/components/SuggesstionBanner.dart';
 import 'package:healthplus/home/components/categories.dart';
 
 import '../components/custom_navigation_bar.dart';
@@ -32,95 +33,96 @@ class Body extends StatelessWidget {
 
         child: Column(
           children: [
-            SizedBox(height: getProportionateScreenWidth(20)),
+            SizedBox(height: getProportionateScreenWidth(25)),
             HomeHeader(),
-            SizedBox(height: getProportionateScreenWidth(10)),
+            SizedBox(height: getProportionateScreenWidth(25)),
             DiscountBanner(),
              Categories(),
-            SizedBox(height: getProportionateScreenWidth(10)),
+            SizedBox(height: getProportionateScreenWidth(25)),
            // CardWidget(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SingleChildScrollView(
-                child: Container(
-                  child: StreamBuilder(
-                    stream: FirebaseFirestore.instance.collection('users').doc(user!.uid).collection("Result").snapshots(),
-                    builder: (context,AsyncSnapshot<QuerySnapshot> snapshot){
-
-                      if(snapshot.hasData){
-                        return ListView.builder(
-                            itemCount: snapshot.data!.docs.length,
-                            itemBuilder: (context,index){
-                              var data=snapshot.data!.docs[index].data() as Map<String,dynamic>;
-                              return Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: SingleChildScrollView(
-                                  child: Container(
-                                    //margin: const EdgeInsets.all(10),
-                                    padding: const EdgeInsets.all(10),
-                                    // height: 200,
-                                    // color: Colors.amberAccent,
-                                    height: MediaQuery.of(context).size.height *1/4,
-                                    width: MediaQuery.of(context).size.height *1/3,
-                                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),border: Border.all(width: 2, color: Colors.teal),color: Colors.teal[50]),
-
-
-                                    child: SingleChildScrollView(
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(left: 5,top: 8.0),
-                                        child: Column(
-                                          //mainAxisAlignment: MainAxisAlignment.center,
-
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              " Name :"+snapshot.data?.docs[index]['Patient Name'],
-                                              style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 15),),
-                                            const SizedBox(
-                                              height: 5,
-                                            ),
-
-                                            Text(
-                                              "Age & Gender :"+snapshot.data?.docs[index]['Age & Sex'],
-                                              style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 15),
-                                            ),
-                                            const SizedBox(
-                                              height: 5,
-                                            ),
-                                            // Text(
-                                            //   "Location :"+snapshot.data?.docs[index]['location'],
-                                            //   style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 15),
-                                            // ),
-                                            const SizedBox(
-                                              height: 5,
-                                            ),
-                                            Text(
-                                              "Contact no :"+snapshot.data?.docs[index]['mobileno'],
-                                              style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 15),
-                                            ),
-                                            const SizedBox(
-                                              height: 5,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              );
-                              return Container();
-                            }
-
-                        );
-
-                      }
-                      return Container();
-
-                    },
-                  ),
-                ),
-              ),
-            ),
+           //  Padding(
+           //    padding: const EdgeInsets.all(8.0),
+           //    child: SingleChildScrollView(
+           //      child: Container(
+           //        child: StreamBuilder(
+           //          stream: FirebaseFirestore.instance.collection('users').doc(user!.uid).collection("Result").snapshots(),
+           //          builder: (context,AsyncSnapshot<QuerySnapshot> snapshot){
+           //
+           //            if(snapshot.hasData){
+           //              return ListView.builder(
+           //                  itemCount: snapshot.data!.docs.length,
+           //                  itemBuilder: (context,index){
+           //                    var data=snapshot.data!.docs[index].data() as Map<String,dynamic>;
+           //                    return Padding(
+           //                      padding: const EdgeInsets.all(10.0),
+           //                      child: SingleChildScrollView(
+           //                        child: Container(
+           //                          //margin: const EdgeInsets.all(10),
+           //                          padding: const EdgeInsets.all(10),
+           //                          // height: 200,
+           //                          // color: Colors.amberAccent,
+           //                          height: MediaQuery.of(context).size.height *1/4,
+           //                          width: MediaQuery.of(context).size.height *1/3,
+           //                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),border: Border.all(width: 2, color: Colors.teal),color: Colors.teal[50]),
+           //
+           //
+           //                          child: SingleChildScrollView(
+           //                            child: Padding(
+           //                              padding: const EdgeInsets.only(left: 5,top: 8.0),
+           //                              child: Column(
+           //                                //mainAxisAlignment: MainAxisAlignment.center,
+           //
+           //                                crossAxisAlignment: CrossAxisAlignment.start,
+           //                                children: [
+           //                                  Text(
+           //                                    " Name :"+snapshot.data?.docs[index]['Patient Name'],
+           //                                    style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 15),),
+           //                                  const SizedBox(
+           //                                    height: 5,
+           //                                  ),
+           //
+           //                                  Text(
+           //                                    "Age & Gender :"+snapshot.data?.docs[index]['Age & Sex'],
+           //                                    style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 15),
+           //                                  ),
+           //                                  const SizedBox(
+           //                                    height: 5,
+           //                                  ),
+           //                                  // Text(
+           //                                  //   "Location :"+snapshot.data?.docs[index]['location'],
+           //                                  //   style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 15),
+           //                                  // ),
+           //                                  const SizedBox(
+           //                                    height: 5,
+           //                                  ),
+           //                                  Text(
+           //                                    "Contact no :"+snapshot.data?.docs[index]['mobileno'],
+           //                                    style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 15),
+           //                                  ),
+           //                                  const SizedBox(
+           //                                    height: 5,
+           //                                  ),
+           //                                ],
+           //                              ),
+           //                            ),
+           //                          ),
+           //                        ),
+           //                      ),
+           //                    );
+           //                    return Container();
+           //                  }
+           //
+           //              );
+           //
+           //            }
+           //            return Container();
+           //
+           //          },
+           //        ),
+           //      ),
+           //    ),
+           //  ),
+            EcommerceImage(),
 
             SizedBox(height: getProportionateScreenWidth(30)),
             //PopularProducts(),
